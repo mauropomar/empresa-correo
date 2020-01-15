d3f82ff7d37f29517c8ee909546978c7d9f81d60
Tony <antonio.navarro@gmail.com>
2020-01-14 23:41:55 -0500

    Creacion de la vista reportes

--- empresa-app/src/app/components/reportes/form-reportes.component.ts
+++ empresa-app/src/app/components/reportes/form-reportes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {AccidenteModel} from "../../models/accidente.model";
 
 @Component({
   selector: 'app-form-reportes',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-reportes.component.css']
 })
 export class FormReportesComponent implements OnInit {
-
+  fechaInicio:Date = new Date();
+  fechaFin:Date = new Date();
+  fechaActual:Date = new Date();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  filterFecha(){
+
   }
 
 }
